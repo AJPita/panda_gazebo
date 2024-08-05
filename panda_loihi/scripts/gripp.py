0ba04448df5baf78c202639735447bfc91605953
AJPita <jaimespita15@gmail.com>
2024-08-05 16:41:01 -0600

    added server action detection

--- panda_loihi/scripts/gripp.py
+++ panda_loihi/scripts/gripp.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+import rospy
+import actionlib
+from franka_gripper.msg import MoveActionGoal,MoveAction,GraspActionGoal,GraspAction 
+from franka_gripper.msg import MoveGoal,GraspGoal
+
 import roslib
 import rospy
 import tf
@@ -17,39 +22,7 @@ from moveit_commander.conversions import pose_to_list
 import time
 
 
-def init_pose():
-    waypoints = []
-    scale=1
-    wpose = move_group.get_current_pose().pose
-    wpose.position.z -= scale * 0.1  # First move up (z)
-    wpose.position.y += scale * 0.2  # and sideways (y)
-    waypoints.append(copy.deepcopy(wpose))
-
-    wpose.position.x += scale * 0.1  # Second move forward/backwards in (x)
-    waypoints.append(copy.deepcopy(wpose))
-
-    wpose.position.y -= scale * 0.1  # Third move sideways (y)
-    waypoints.append(copy.deepcopy(wpose))
-
-    # We want the Cartesian path to be interpolated at a resolution of 1 cm
-    # which is why we will specify 0.01 as the eef_step in Cartesian
-    # translation.  We will disable the jump threshold by setting it to 0.0,
-    # ignoring the check for infeasible jumps in joint space, which is sufficient
-    # for this tutorial.
-    (plan, fraction) = move_group.compute_cartesian_path(
-                                    waypoints,   # waypoints to follow
-                                    0.01,        # eef_step
-                                    0.0)         # jump_threshold
-
-    # Note: We are just planning, not asking move_group to actually move the robot yet:
-    move_group.execute(plan, wait=True)
-    
-
-
-
-
-if __name__=="__main__":
-    rospy.init_node("lsitener_chip")
+def pre_move():
     listener=tf.TransformListener()   
     moveit_commander.roscpp_initialize(sys.argv)
 
@@ -84,13 +57,17 @@ if __name__=="__main__":
     scale=1
     wpose.position.x =trans1[0]
     wpose.position.y =trans1[1]
-    wpose.position.z =trans1[2]+0.15
+    wpose.position.z =trans1[2]+0.12
+    waypoints.append(copy.deepcopy(wpose))
+   
+    wpose.position.z = wpose.position.z +0.35
+    waypoints.append(copy.deepcopy(wpose))
     print("moving to desired position")
     print(wpose)
-    waypoints.append(copy.deepcopy(wpose))
+   
     wpose.position.x =trans2[0] # Third move sideways (y)
-    wpose.position.y =trans2[1] 
-    wpose.position.z =trans2[2]+0.15
+    wpose.position.y =trans2[1]-0.012 
+    wpose.position.z =trans2[2]+0.12
     waypoints.append(copy.deepcopy(wpose))
    
     print(waypoints)
@@ -111,5 +88,48 @@ if __name__=="__main__":
 
     # Note: We are just planning, not asking move_group to actually move the robot yet:
     move_group.execute(plan, wait=True)
-    
 
+
+def pre_pick():
+    client.wait_for_server()
+    goal=MoveActionGoal()
+    g=MoveGoal()
+    g.width=0.08
+    g.speed=0.1
+    goal=g
+    client.send_goal(goal)
+    client.wait_for_result()
+
+def pick():
+    client2.wait_for_server()
+   
+    goal2=GraspActionGoal()
+    
+    gr=GraspGoal()
+   
+    gr.width,=0.03,
+    gr.epsilon.inner=0.005
+    gr.epsilon.outer= 0.005
+    gr.speed=0.1
+    gr.force=5.0
+    print(gr)
+    
+    goal2=gr
+    
+    client2.send_goal(goal2)
+    client2.wait_for_result()
+
+
+
+if __name__=='__main__':
+    rospy.init_node('pick')
+    client=actionlib.SimpleActionClient('/franka_gripper/move',MoveAction)
+    client2=actionlib.SimpleActionClient('/franka_gripper/move',MoveAction)
+   
+    pre_move()
+    pre_pick()
+    pick()
+
+
+#rostopic pub --once /franka_gripper/move/goal franka_gripper/MoveActionGoal "goal: { width: 0.08, speed: 0.1 }"
+#rostopic pub --once /franka_gripper/grasp/goal franka_gripper/GraspActionGoal \ "goal: { width: 0.03, epsilon:{ inner: 0.005, outer: 0.005 }, speed: 0.1, force: 5.0}"
